a57e558d750d23de0955ac79768389d379b6ba15
Stefano Zanella <zanella.stefano@gmail.com>
2022-09-27 19:08:40 +0200

    Prepare for publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,11 @@ plugins {
   `java-gradle-plugin`
   `kotlin-dsl`
   kotlin("jvm") version "1.6.21"
+  id("com.gradle.plugin-publish") version "1.0.0"
 }
+
+group = "me.stefanozanella"
+version = "0.0.1"
 
 repositories {
   mavenCentral()
@@ -32,5 +36,13 @@ gradlePlugin {
   val plugin by plugins.creating {
     id = "me.stefanozanella.gradle.plugin.k8s-deploy"
     implementationClass = "me.stefanozanella.gradle.plugin.k8sdeploy.K8sDeployPlugin"
+    displayName = "Kubernetes Deployment Gradle Plugin"
+    description = "Automatically build and deploy a Docker image for your project to an existing Kubernetes deployment"
   }
+}
+
+pluginBundle {
+  website = "https://github.com/stefanozanella/gradle-plugin-k8s-deploy"
+  vcsUrl = "https://github.com/stefanozanella/gradle-plugin-k8s-deploy.git"
+  tags = listOf("kubernetes", "docker", "deployment")
 }
